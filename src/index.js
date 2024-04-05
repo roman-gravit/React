4a55f86646806ecc4974d382ac645d1d7471ce0d
Roman Gravit <romang@siber.com>
2024-04-05 12:57:13 +0100

    React. API calls

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { StrictMode } from 'react';
-import { Lesson } from "./15-context/Lesson";
+import { News } from "./16-api/Lesson";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <StrictMode>
-        <Lesson />
+        <News />
     </StrictMode>
 
 );
