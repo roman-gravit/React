0643f1c70b324f9eb475d604d2ccbbfd9002ff2f
Roman Gravit <romang@siber.com>
2024-04-05 15:53:41 +0100

    Router. Part 1.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { StrictMode } from 'react';
-import { News } from "./16-api/Lesson";
+import { App } from "./18-router/Lesson";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+
+import Home from './18-router/components/home';
+import Contacts from './18-router/components/contacts';
+import Posts from './18-router/components/posts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <StrictMode>
-        <News />
+        <BrowserRouter>
+            <App>
+                <Routes>
+                    <Route exact path='/' component={Home} />
+                    <Route path='/contacts' component={Contacts} />
+                    <Route exact path='/posts' component={Posts} />
+                </Routes>
+            </App>
+        </BrowserRouter>
     </StrictMode>
 
 );
